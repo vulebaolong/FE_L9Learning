251ba105eadee5a4e91b1f0b362c6b679e602db0
vulebaolong <vulebaolong@gmail.com>
2023-08-23 19:21:45 +0700

    fix BASE_URL

--- src/contants/apiContants.tsx
+++ src/contants/apiContants.tsx
@@ -1,3 +1,3 @@
 const isProduction = import.meta.env.PROD;
 
-export const BASE_URL = isProduction ? "https://l9-learning-api-vulebaolong.vercel.app/api/v1" : "http://localhost:3001/api/v1";
+export const BASE_URL = isProduction ? "https://l9-learning.onrender.com/api/v1" : "http://localhost:3001/api/v1";
