1a6dd97383706de67a553bbac68a05bd211465da
vulebaolong <vulebaolong@gmail.com>
2023-08-23 22:59:40 +0700

    fix header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { MdLightMode, MdNightlight } from "react-icons/md";
-import { useDispatch } from "react-redux";
-import { DispatchType } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { DispatchType, RootState } from "../../redux/store";
 import { toggleThemeREDU } from "../../redux/slices/toggleThemeSlice";
 
 import UserControll from "./UserControll";
@@ -10,6 +10,8 @@ import Search from "../Search/Search";
 import NavbarMobile from "./Mobile/NavbarMobile";
 
 function Header() {
+    const { userLogin } = useSelector((state: RootState) => state.quanLyNguoiDungSlice);
+
     const btnLightRef = useRef<HTMLDivElement | null>(null);
     const btnNightRef = useRef<HTMLDivElement | null>(null);
 
@@ -20,31 +22,59 @@ function Header() {
     };
 
     return (
-        <div className="bg-white dark:bg-slate-900 sticky left-0 top-0 right-0 z-20 h-header px-7 flex items-center justify-between border-b dark:border-slate-700 border-slate-200">
+        <>
             {/* MOBILE */}
-            <div className="block lg:hidden">
-                <NavbarMobile />
-            </div>
-
-            <div className="hidden lg:flex items-center gap-2">
-                <Logo />
-                <h4 className="text-sm font-bold">Học Lập Trình Để Đi Làm</h4>
-            </div>
-
-            <div className="hidden md:block">
-                <Search />
-            </div>
-
-            <div className="flex items-center gap-2">
-                <div ref={btnLightRef} onClick={toggleTheme} className="btnLight text-2xl cursor-pointer">
-                    <MdLightMode />
+            <div className="flex lg:hidden bg-white dark:bg-slate-900 sticky left-0 top-0 right-0 z-20 h-header px-7  items-center justify-between border-b dark:border-slate-700 border-slate-200">
+                {/* NAV */}
+                <div className="">
+                    <NavbarMobile />
                 </div>
-                <div ref={btnNightRef} onClick={toggleTheme} className="btnNight text-2xl hidden cursor-pointer">
-                    <MdNightlight />
+
+                {/* LOGO */}
+                {userLogin && (
+                    <div className="items-center gap-2">
+                        <Logo />
+                        <h4 className="hidden lg:flex text-sm font-bold">Học Lập Trình Để Đi Làm</h4>
+                    </div>
+                )}
+
+                {/* TOOGLE THEME */}
+                <div className="flex items-center gap-2">
+                    <div ref={btnLightRef} onClick={toggleTheme} className="btnLight text-2xl cursor-pointer">
+                        <MdLightMode />
+                    </div>
+                    <div ref={btnNightRef} onClick={toggleTheme} className="btnNight text-2xl hidden cursor-pointer">
+                        <MdNightlight />
+                    </div>
+                    <UserControll />
                 </div>
-                <UserControll />
             </div>
-        </div>
+
+            {/* DESKTOP */}
+            <div className="hidden lg:flex bg-white dark:bg-slate-900 sticky left-0 top-0 right-0 z-20 h-header px-7  items-center justify-between border-b dark:border-slate-700 border-slate-200">
+                {/* LOGO */}
+                <div className="flex items-center gap-2">
+                    <Logo />
+                    <h4 className="text-sm font-bold">Học Lập Trình Để Đi Làm</h4>
+                </div>
+
+                {/* SEARCH */}
+                <div className="hidden md:block">
+                    <Search />
+                </div>
+
+                {/* TOOGLE THEME */}
+                <div className="flex items-center gap-2">
+                    <div ref={btnLightRef} onClick={toggleTheme} className="btnLight text-2xl cursor-pointer">
+                        <MdLightMode />
+                    </div>
+                    <div ref={btnNightRef} onClick={toggleTheme} className="btnNight text-2xl hidden cursor-pointer">
+                        <MdNightlight />
+                    </div>
+                    <UserControll />
+                </div>
+            </div>
+        </>
     );
 }
 export default Header;
